e3ab449bcdd734ad6a3b9759a838d129b6b03d0b
Daniel Yount <yountdaniel@gmail.com>
2016-03-01 21:45:37 -0500

    update with contents

--- Stress Manager/AppDelegate.swift
+++ Stress Manager/AppDelegate.swift
@@ -12,33 +12,68 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var shortcutItem: UIApplicationShortcutItem?
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        print("Application did finish launching with options")
+        
+        var performShortcutDelegate = true
+        
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsShortcutItemKey] as? UIApplicationShortcutItem {
+            
+            print("Application launched via shortcut")
+            self.shortcutItem = shortcutItem
+            
+            performShortcutDelegate = false
+        }
+        
+        return performShortcutDelegate
+        
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        
+        print("Application performActionForShortcutItem")
+        completionHandler( handleShortcut(shortcutItem) )
+        
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func handleShortcut( shortcutItem:UIApplicationShortcutItem ) -> Bool {
+        print("Handling shortcut")
+        
+        var succeeded = false
+        
+        if( shortcutItem.type == "be.thenerd.appshortcut.new-user" ) {
+            
+            // Add your code here
+            print("- Handling \(shortcutItem.type)")
+            
+            // Get the view controller you want to load
+            let mainSB = UIStoryboard(name: "Main.storyboard", bundle: nil)
+            let addUserVC = mainSB.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
+            
+            let navVC = self.window?.rootViewController as! UINavigationController
+            navVC.pushViewController(addUserVC, animated: true)
+            
+            succeeded = true
+            
+        }
+        
+        return succeeded
+        
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("Application did become active")
+        
+        guard let shortcut = shortcutItem else { return }
+        
+        print("- Shortcut property has been set")
+        
+        handleShortcut(shortcut)
+        
+        self.shortcutItem = nil
+        
     }
 
 
--- Stress Manager/ViewController.swift
+++ Stress Manager/ViewController.swift
@@ -10,14 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var counter = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func animateButton(sender: AnyObject) {
+        
+        
+        
+        if (counter == 0){
+            imageView.startAnimating()
+            counter += 1
+        } else if (counter == 1){
+            imageView.stopAnimating()
+            counter += 1
+        } else if (counter == 2){
+            imageView.startAnimating()
+            counter += 1
+        } else if (counter == 3){
+            imageView.stopAnimating()
+            counter = 0
+        }
+        
+        imageView.stopAnimating()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        imageView.animationImages = [
+            
+            UIImage(named: "Onboarding 1")!,
+            UIImage(named: "Onboarding 2")!,
+            UIImage(named: "Onboarding 3")!,
+            UIImage(named: "Onboarding 4")!,
+            UIImage(named: "Onboarding 5")!,
+            UIImage(named: "profile view")!,
+            UIImage(named: "dashboard view")!
+            
+        ]
+        
+        imageView.animationDuration = 12
+        imageView.startAnimating()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
     }
 
 
