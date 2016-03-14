453d6cb151747ea8ab43edb6021943428b61359c
Connor Greene <cgree104@uncc.edu>
2016-03-14 09:14:09 -0400

    Working PVC
    built running PVC

---
+++ Stress Manager/PageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PageViewController.swift
+//  Stress Manager
+//
+//  Created by Connor Greene on 3/2/16.
+//  Copyright © 2016 Daniel Yount. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let subViews: NSArray = view.subviews
+        var scrollView: UIScrollView? = nil
+        var pageControl: UIPageControl? = nil
+        
+        for view in subViews {
+            if view.isKindOfClass(UIScrollView) {
+                scrollView = view as? UIScrollView
+            }
+            else if view.isKindOfClass(UIPageControl) {
+                pageControl = view as? UIPageControl
+            }
+        }
+        
+        if (scrollView != nil && pageControl != nil) {
+            scrollView?.frame = view.bounds
+            view.bringSubviewToFront(pageControl!)
+        }
+    }
+}
--- Stress Manager/ViewController.swift
+++ Stress Manager/ViewController.swift
@@ -6,63 +6,111 @@
 //  Copyright © 2016 Daniel Yount. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
-    var counter = 0
+class ViewController: UIViewController, UIPageViewControllerDataSource {
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBAction func animateButton(sender: AnyObject) {
-        
-        
-        
-        if (counter == 0){
-            imageView.startAnimating()
-            counter += 1
-        } else if (counter == 1){
-            imageView.stopAnimating()
-            counter += 1
-        } else if (counter == 2){
-            imageView.startAnimating()
-            counter += 1
-        } else if (counter == 3){
-            imageView.stopAnimating()
-            counter = 0
-        }
-        
-        imageView.stopAnimating()
-        
-    }
+    var pageViewController: UIPageViewController!
+    var pageTitles: NSArray!
+    var pageImages: NSArray!
+    var button: Int!
+    @IBAction
     
-    override func viewDidLoad() {
-        
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        imageView.animationImages = [
-            
-            UIImage(named: "Onboarding 1")!,
-            UIImage(named: "Onboarding 2")!,
-            UIImage(named: "Onboarding 3")!,
-            UIImage(named: "Onboarding 4")!,
-            UIImage(named: "Onboarding 5")!,
-            UIImage(named: "profile view")!,
-            UIImage(named: "dashboard view")!
-            
-        ]
+        self.pageTitles = NSArray(objects: "Onboarding 1", "Onboarding 2", "Onboarding 3", "Onboarding 4", "Onboarding 5", "dashboard view", "profile view")
+        self.pageImages = NSArray(objects: "Onboarding 1", "Onboarding 2", "Onboarding 3", "Onboarding 4", "Onboarding 5", "profile view", "dashboard view")
         
-        imageView.animationDuration = 12
-        imageView.startAnimating()
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
+        self.pageViewController.dataSource = self
+        
+        let startVC = self.viewControllerAtIndex(0) as ContentViewController
+        let viewControllers = NSArray(object: startVC)
+        
+        self.pageViewController.setViewControllers(viewControllers as? [UIViewController], direction: .Forward, animated: true, completion: nil)
+        
+        
+        self.addChildViewController(self.pageViewController)
+        self.view.addSubview(self.pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
     }
     
-    override func prefersStatusBarHidden() -> Bool {
-        return true
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    override func shouldAutorotate() -> Bool {
-        return false
+    func viewControllerAtIndex(index: Int) -> ContentViewController
+    {
+        if ((self.pageTitles.count == 0) || (index >= self.pageTitles.count)) {
+            return ContentViewController()
+        }
+        
+        let vc: ContentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ContentViewController") as! ContentViewController
+        
+        vc.imageFile = self.pageImages[index] as! String
+        vc.titleText = self.pageTitles[index] as! String
+        vc.pageIndex = index
+        
+        return vc
     }
-
-
+    
+    
+    // MARK: - Page View Controller Data Source
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
+    {
+        
+        let vc = viewController as!ContentViewController
+        var index = vc.pageIndex as Int
+        
+        
+        if (index == 0 || index == NSNotFound)
+        {
+            return nil
+            
+        }
+        
+        index--
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let vc = viewController as! ContentViewController
+        var index = vc.pageIndex as Int
+        
+        if (index == NSNotFound)
+        {
+            return nil
+        }
+        
+        index++
+        
+        if (index == self.pageTitles.count)
+        {
+            return nil
+        }
+                
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int
+    {
+        return self.pageTitles.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int
+    {
+        return 0
+    }
+    
+    
+    
 }
-
